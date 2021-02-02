5fb3c61fe1661bc7fbed27ee2f3572ba3779d540
Avishek Majumder <avishekmojo212@gmail.com>
2021-02-02 19:42:35 +0600

    first code

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//when condition will be false --
+// 0
+// ""
+// undefined
+// null
+// NaN
+//false
+
+//when condition will be true --
+//'0' ' ' []
+// 'false'
+
+
+const name = "solaiman";
+
+if(name.length > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
